0d2aea680675cf9025e5780f16b5947164c3db99
rajeevloghade <rajeevloghade010@gmail.com>
2020-10-07 01:08:48 +0530

    added

---
+++ EmployeeManagement/src/test/java/EmployeeManagement/EmployeeManagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package EmployeeManagement.EmployeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
